6f9d651cc493a794d09d5235bf49422a8fb4dfe3
Antonis <papanton@usc.edu>
2015-10-20 15:04:32 -0700

    multi again. try number 1231312

---
+++ BowGame/AppWarpHelperClasses/AppWarpHelper.swift
@@ -0,0 +1,141 @@
+//
+//  AppWarpHelper.swift
+//  FightNinja
+//
+//  Created by Shephertz on 20/06/14.
+//  Copyright (c) 2014 Shephertz. All rights reserved.
+//
+
+import UIKit
+
+
+class AppWarpHelper: NSObject
+{
+
+    var api_key = "cad2bfab6310acd9696187b98682925125e469ab0d0d585db0b00609f461b791"
+    var secret_key = "55811709916e7ce4405cde0cdc5a254cf4b506fbafdae05760a73100b8080b67"
+    var roomId = ""
+    var enemyName: String = ""
+    var playerName: String = ""
+    
+    var startGameScene: StartGameScene? = nil
+    var gameScene: GameScene? = nil
+    
+    class var sharedInstance:AppWarpHelper{
+        struct Static{
+            static var instance:AppWarpHelper?
+            static var token: dispatch_once_t = 0
+            }
+            
+            dispatch_once(&Static.token){
+                Static.instance = AppWarpHelper()
+            }
+            return Static.instance!
+    }
+
+    func initializeWarp()
+    {
+        WarpClient.initWarp(api_key, secretKey: secret_key)
+        let warpClient:WarpClient = WarpClient.getInstance()
+        warpClient.enableTrace(true)
+        warpClient.setRecoveryAllowance(60);
+        
+        let connectionListener: ConnectionListener = ConnectionListener()
+        warpClient.addConnectionRequestListener(connectionListener)
+        
+        
+        let zoneListener: ZoneListener = ZoneListener()
+        warpClient.addZoneRequestListener(zoneListener)
+        
+        let notificationListener:NotificationListener = NotificationListener()
+        warpClient.addNotificationListener(notificationListener)
+        
+        let roomListener: RoomListener = RoomListener()
+        warpClient.addRoomRequestListener(roomListener)
+    }
+    
+    func connectWithAppWarpWithUserName(userName:String)
+    {
+        let uNameLength = userName.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)
+        if uNameLength>0
+        {
+            let warpClient:WarpClient = WarpClient.getInstance()
+            warpClient.connectWithUserName(userName)
+        }
+    }
+    
+    
+    func updatePlayerDataToServer(dataDict:NSMutableDictionary)
+    {
+        if dataDict == NSNull()
+        {
+            return
+        }
+        print(dataDict)
+        let convertedData = try? NSJSONSerialization.dataWithJSONObject(dataDict, options: NSJSONWritingOptions.PrettyPrinted)
+        //var convertedData = NSPropertyListSerialization.dataWithPropertyList(dataDict, format: NSPropertyListFormat.XMLFormat_v1_0, options: 0, error: nil)
+        
+        if convertedData == nil
+        {
+            return
+        }
+        
+        if WarpClient.getInstance().getConnectionState()==0
+        {
+            print("updatePlayerDataToServer")
+            WarpClient.getInstance().sendUpdatePeers(convertedData)
+        }
+    }
+    
+    func receivedEnemyStatus(data:NSData)
+    {
+        print("receivedEnemyStatus...1")
+        print(data)
+
+        if data == NSNull()
+        {
+            print(data)
+        }
+        else
+        {
+            print("receivedEnemyStatus...2")
+            
+            let error: NSError? = nil;
+            let responseDict: NSMutableDictionary = (try! NSJSONSerialization.JSONObjectWithData(data,options: NSJSONReadingOptions.MutableContainers)) as! NSMutableDictionary
+            
+            //var propertyListFormat:NSPropertyListFormat? = nil
+            //var responseDict: NSDictionary = NSPropertyListSerialization.propertyListWithData(data, options: 0, format:&propertyListFormat, error: nil) as NSDictionary
+            print(responseDict)
+            //println(error!.value)
+            //gameScene!.updateEnemyStatus(responseDict)
+            
+            if (error == nil)
+            {
+                print("receivedEnemyStatus...3")
+                
+                if enemyName.isEmpty
+                {
+                    let userName : (String!) = responseDict.objectForKey("userName") as! String
+                    let isEqual = playerName.hasPrefix(userName)
+                    if !isEqual
+                    {
+                        enemyName = responseDict.objectForKey("userName") as! String
+                        gameScene!.updateEnemyStatus(responseDict)
+                    }
+                }
+                else
+                {
+                    
+                    let userName : (String!) = responseDict.objectForKey("userName") as! String
+                    let isEqual = enemyName.hasPrefix(userName)
+                    if isEqual
+                    {
+                        gameScene!.updateEnemyStatus(responseDict)
+                    }
+                }
+            }
+
+        }
+        
+    }
+}
--- BowGame/StartGameScene.swift
+++ BowGame/StartGameScene.swift
@@ -12,6 +12,10 @@ import Darwin
 
 class StartGameScene: SKScene {
     
+    var textField: UITextField!
+    let playerName = "test"
+
+    
     let buttonnames = ["Stages", "Start", "Resume", "Settings","Quit"]
     let buttonfuncs = ["Stages": {(s:StartGameScene)->Void in s.startStage()},
         "Start" : {(s:StartGameScene)->Void in s.startGame()},
@@ -20,6 +24,16 @@ class StartGameScene: SKScene {
         "Quit" : {(s:StartGameScene)->Void in exit(0)}]
     
     var current_game : SKScene?
+
+    func addTextField() {
+        let screensize = UIScreen.mainScreen().bounds.size;
+        
+        textField = UITextField(frame : CGRect(x:20, y:(screensize.height/2.25), width:(screensize.width/10), height:(screensize.height/15) ))
+        self.view!.addSubview(textField)
+        textField.backgroundColor = UIColor.blueColor()
+        textField.textAlignment = .Center
+        textField.font = UIFont(name: "Helvetica Neue", size: 23)
+    }
 
     func createButton(name : String, position : CGPoint)->SKNode
     {
@@ -75,19 +89,31 @@ class StartGameScene: SKScene {
     
     func startGame()
     {
+        let name = textField.text!
+        
+        textField.resignFirstResponder()
+
         let screensize = UIScreen.mainScreen().bounds.size;
         let scenesize : CGSize = CGSize(width: screensize.width, height: screensize.height)
-        let gameScene = MutiplayerScene(size: scenesize, mainmenu: self)
+
+        let gameScene = MutiplayerScene(size: scenesize, mainmenu: self, localPlayer: name)
         gameScene.scaleMode = SKSceneScaleMode.AspectFit
+        textField.removeFromSuperview()
+        AppWarpHelper.sharedInstance.gameScene = gameScene
+
         changeScene(gameScene)
     }
     
     func startStage()
     {
+        let playerName = "temp"
         let screensize = UIScreen.mainScreen().bounds.size;
         let scenesize : CGSize = CGSize(width: screensize.width, height: screensize.height)
-        let gameScene = StageGameScene(size: scenesize, mainmenu: self)
+        
+        let gameScene = StageGameScene(size: scenesize, mainmenu: self, localPlayer: playerName )
         gameScene.scaleMode = SKSceneScaleMode.AspectFit
+        textField.removeFromSuperview()
+
         changeScene(gameScene)
     }
     
@@ -97,8 +123,15 @@ class StartGameScene: SKScene {
         view?.presentScene(scene,transition: transitionType)
     }
     override func didMoveToView(view: SKView) {
+        addTextField()
+        
+        AppWarpHelper.sharedInstance.initializeWarp()
+        AppWarpHelper.sharedInstance.startGameScene = self
+        
+        
     }
     
+
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch = touches.first!
         let touchLocation = touch.locationInNode(self)
