77ca141b6b28bf2249c7f19ea5ff14aab9425f0a
John-zhy <zhan399@usc.edu>
2015-10-20 17:06:03 -0700

    change start game scene

--- BowGame/StartGameScene.swift
+++ BowGame/StartGameScene.swift
@@ -12,27 +12,23 @@ import Darwin
 
 class StartGameScene: SKScene {
     
-    let buttonnames = ["Stages", "Start", "Resume", "Settings","Quit"]
-    let buttonfuncs = ["Stages": {(s:StartGameScene)->Void in s.startStage()},
-        "Start" : {(s:StartGameScene)->Void in s.startGame()},
-        "Resume" : {(s:StartGameScene)->Void in s.resume()},
+    let buttonnames = ["Single", "Multiple", "Settings", "Exit"]
+    let buttonImage = ["Single" : "singleplayerbutton", "Multiple" : "multipleplayerbutton", "Settings" : "settingsbutton","Exit" : "exitbutton"]
+    let buttonfuncs = ["Single": {(s:StartGameScene)->Void in s.startStage()},
+        "Multiple" : {(s:StartGameScene)->Void in s.startGame()},
+        //"Resume" : {(s:StartGameScene)->Void in s.resume()},
         "Settings" : {(s:StartGameScene)->Void in s.settings()},
-        "Quit" : {(s:StartGameScene)->Void in exit(0)}]
+        "Exit" : {(s:StartGameScene)->Void in exit(0)}]
     
     var current_game : SKScene?
 
     func createButton(name : String, position : CGPoint)->SKNode
     {
-        let text : SKLabelNode = SKLabelNode()
-        text.name = name
-        text.text = name
-        text.fontColor = SKColor.blackColor()
-        text.fontSize = 50
-        text.fontName = "MarkerFelt-Wide"
-        text.zPosition = 1
-        text.position = position
-        text.horizontalAlignmentMode = SKLabelHorizontalAlignmentMode.Left
-        return text
+        let button = SKSpriteNode(imageNamed: buttonImage[name]!)
+        button.name = name
+        button.zPosition = 2
+        button.position = position
+        return button
     }
     func settings()
     {
@@ -47,27 +43,35 @@ class StartGameScene: SKScene {
             view?.presentScene(self.current_game!,transition: transitionType)
         }
     }
-    
+    func addBackground()
+    {
+        let background = SKSpriteNode(imageNamed: BackgroundImage)
+        background.position = CGPoint(x: 0.0, y: background.size.height/2)
+        addChild(background)
+    }
 
     override init(size: CGSize) {
         super.init(size: size)
+        addBackground()
         addButtons()
-        self.backgroundColor = UIColor.whiteColor()
-       
     }
     func addButtons()
     {
         var i = 1;
-        let left = (size.width*0.4)
+        let left = (size.width*0.8)
         for name in buttonnames{
             print(name)
-            let top =  size.height*CGFloat(1.0-0.18*CGFloat(i))
+            let top =  size.height*(1 -  0.2 * CGFloat(i))
             let position = CGPointMake(left ,  top)
             let button = createButton(name, position: position)
             addChild(button)
+            print(i)
             ++i
         }
-
+        let stick = SKSpriteNode(imageNamed: "startscenestick")
+        stick.zPosition = 1
+        stick.position = CGPointMake(left, size.height/2)
+        addChild(stick)
     }
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
@@ -96,8 +100,6 @@ class StartGameScene: SKScene {
         let transitionType = SKTransition.flipHorizontalWithDuration(1.0)
         view?.presentScene(scene,transition: transitionType)
     }
-    override func didMoveToView(view: SKView) {
-    }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
         let touch = touches.first!
@@ -106,7 +108,7 @@ class StartGameScene: SKScene {
         if (touchedNode.name != nil){
             buttonfuncs[touchedNode.name!]?(self)
         }
-
+        
        
     }
     
