508de808e4abcd59ce443f0a9c671a158541d119
eko910817 <zqiliang0817@gmail.com>
2015-10-24 14:05:29 -0700

    multiplayer bug fixed
    problem with clear player name and enemy name

--- BowGame/AppWarpHelperClasses/AppWarpHelper.swift
+++ BowGame/AppWarpHelperClasses/AppWarpHelper.swift
@@ -79,8 +79,8 @@ class AppWarpHelper: NSObject
         warpClient.deleteRoom(roomId)
         warpClient.disconnect()
         self.roomId = ""
-//        self.playerName = ""
-//        self.enemyName = ""
+        self.playerName = ""
+        self.enemyName = ""
         }
     }
     
@@ -172,6 +172,7 @@ class AppWarpHelper: NSObject
                     
                     let transitionType = SKTransition.flipHorizontalWithDuration(1.0)
                     gameScene?.view?.presentScene((gameScene?.mainmenu)!, transition: transitionType)
+                    
                     disconnectFromServer()
                     
                     return
