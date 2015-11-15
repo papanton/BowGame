6c965a7f24c88c1b7d078167da68d52a83660ba6
John-zhy <zhan399@usc.edu>
2015-11-15 14:37:16 -0800

    New design for StageFour
    New design for StageFour

--- BowGame/StageSix.swift
+++ BowGame/StageSix.swift
@@ -8,7 +8,26 @@
 //
 
 import UIKit
-
-class StageSix: StageGameScene {
-
+import SpriteKit
+class StageSix: StageGameScene
+{
+    override func addBackground()
+    {
+        let backgroundTexture =  SKTexture(imageNamed:"MoonBackground")
+        let background = SKSpriteNode(texture:backgroundTexture, color: SKColor.clearColor(), size: CGSizeMake(self.frame.width * 2, self.frame.height))
+        
+        background.zPosition = -100;
+        background.position = CGPointMake(size.width,  size.height*0.5)
+        //background.size = world.frame.size
+        self.world.addChild(background)
+    }
+    override func addObstacle(){
+        addRock(CGPointMake(self.size.width * 1.1, size.height - 100))
+    }
+    func addRock(position : CGPoint)
+    {
+        let rock : Rock = Rock(position: position)
+        self.world.addChild(rock)
+    }
+    override func addGround(){}
 }
